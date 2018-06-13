8cef55252e3c7e1039621921a141cf0fab561d44
huangkaibin <huangkaibin@seedland.cc>
2018-06-13 20:44:30 +0800

    add spring bus

---
+++ mircoservices/cmds-service/src/main/java/com/kayb/cmds/configuration/RefreshBean.java
@@ -0,0 +1,13 @@
+package com.kayb.cmds.configuration;
+
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author huangkaibin
+ * @date 2018-06-13
+ **/
+@RefreshScope
+@Configuration
+public class RefreshBean {
+}
--- mircoservices/cmds-service/src/main/java/com/kayb/cmds/controller/CustomerController.java
+++ mircoservices/cmds-service/src/main/java/com/kayb/cmds/controller/CustomerController.java
@@ -4,6 +4,8 @@ import com.kayb.cmds.domain.Customer;
 import com.kayb.cmds.domain.Properties;
 import com.kayb.cmds.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,11 +15,20 @@ import java.util.List;
  * @author huangkaibin
  * @date 2018-05-29
  **/
+@RefreshScope
 @RestController
 public class CustomerController {
 
+    @Value("${profile}")
+    private String profile;
+
     @Autowired
     private CustomerService customerService;
+
+    @GetMapping("/refresh")
+    public String refresh() {
+        return profile;
+    }
 
     @GetMapping("/1.0.0/cust/getCustInfo")
     public Customer custInfo(String mobile) {
