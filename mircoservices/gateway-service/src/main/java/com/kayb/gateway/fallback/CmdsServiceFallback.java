8c2f650b0ebd6c0c94d7aa6328d43ae3956bba3a
huangkaibin <huangkaibin@seedland.cc>
2018-06-14 16:19:24 +0800

    add fallback provider for zuul gateway

---
+++ mircoservices/gateway-service/src/main/java/com/kayb/gateway/fallback/CmdsServiceFallback.java
@@ -0,0 +1,63 @@
+package com.kayb.gateway.fallback;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.springframework.cloud.netflix.zuul.filters.route.FallbackProvider;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author huangkaibin
+ * @date 2018-06-14
+ **/
+@Component
+public class CmdsServiceFallback implements FallbackProvider {
+
+    @Override
+    public String getRoute() {
+        return "passport-service";
+    }
+
+    @Override
+    public ClientHttpResponse fallbackResponse(String route, Throwable cause) {
+        return new ClientHttpResponse() {
+
+            @Override
+            public HttpHeaders getHeaders() {
+                HttpHeaders headers = new HttpHeaders();
+                headers.setContentType(MediaType.APPLICATION_JSON);
+                return headers;
+            }
+
+            @Override
+            public InputStream getBody() throws IOException {
+                return new ByteArrayInputStream("fallback".getBytes());
+            }
+
+            @Override
+            public String getStatusText() throws IOException {
+                return "OK";
+            }
+
+            @Override
+            public HttpStatus getStatusCode() throws IOException {
+                return HttpStatus.OK;
+            }
+
+            @Override
+            public int getRawStatusCode() throws IOException {
+                return 200;
+            }
+
+            @Override
+            public void close() {
+
+            }
+        };
+    }
+}
